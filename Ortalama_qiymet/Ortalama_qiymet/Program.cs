15816c32efbf5a0d190c8e66c6f6d3c06cbf7c1b
Turan047 <hasanli_2ran@hotmail.com>
2017-06-19 12:19:59 +0400

    Add files via upload
    average grade

---
+++ Ortalama_qiymet/Ortalama_qiymet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ortalama_qiymet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your midterm grade: ");
+            double midterm = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your grade from final exam: ");
+            double final = Convert.ToInt32(Console.ReadLine());
+
+            double final_gr = midterm * 0.4 + final * 0.6;
+
+            if (final_gr>=45)
+            {
+                Console.WriteLine("You passed the course,your average grade is: " + final_gr);
+            }
+            else
+            {
+                Console.WriteLine("You failed the course, your average grade is: " + final_gr);
+            }
+        }
+    }
+}
---
+++ Ortalama_qiymet/Ortalama_qiymet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Ortalama_qiymet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ortalama_qiymet")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1f4c3953-26bb-49eb-8b7a-8b0e866b680c")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
